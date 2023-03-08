005f260dc8c753efb906ff19c02c9f581865b25c
Wilsantos1975 <Faustosantos@pursuit.org>
2023-03-07 21:32:02 -0500

    refactored function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function fn(...c) {
-  if (!c.every((a) => Number.isNumber(a)))
-    throw "All arguments must be numbers.";
-  return c.reduce((a, b) => a + b);
-}
+    const areAllNumbers = c.every((a) => Number.isNumber(a));
+    if (!areAllNumbers) {
+      throw new Error("All arguments must be numbers.");
+    }
+    return c.reduce((a, b) => a + b);
+  }
+  
+  fn(1, 2, 3); //> 6
+  fn(10, "B", 20); //> error All arguments must be numbers.
 
-fn(1, 2, 3); //> 6
-fn(10, "B", 20); //> error All arguments must be numbers.
